54bcb86341324aa0e5587c7f3b441a492cd1c4ee
Ella_Q_Kotlin <baellap8@gmail.com>
2024-07-01 18:17:54 -0700

    ** stage 2 of hyperskill pawnsOnlyChess ** adds game loops to alternate moves made by players, and checkValidMove() fitting pattern of any two valid chess squares i.e. a2a3 (even illegal moves)

--- src/main/kotlin/pawns_only_chess.kt
+++ src/main/kotlin/pawns_only_chess.kt
@@ -27,12 +27,58 @@ fun main() {
         if (rank == 6) token = "W"
         token
     } }
+    playGame(gameBoard)
 
-    printGameBoard(gameBoard)
 }
 
-fun printGameBoard(board: List<MutableList<String>>) {
+fun playGame(board: List<MutableList<String>>) {
+    printTitle()
+    val firstPlayer = promptPlayer("First")
+    val secondPlayer = promptPlayer("Second")
+    printGameBoard(board)
+    var count = 0
+    outer@while (true) {
+        if (count % 2 == 0) {
+            while (true) {
+                println("$firstPlayer's turn:")
+                val response = readln()
+                if (response == "exit") {
+                    println("Bye!")
+                    break@outer
+                }
+                val valid = checkValidMove(response)
+                if (!valid) println("Invalid Input") else break
+            }
+        } else {
+            while (true) {
+                println("$secondPlayer's turn:")
+                val response = readln()
+                if (response == "exit") {
+                    println("Bye!")
+                    break@outer
+                }
+                val valid = checkValidMove(response)
+                if (!valid) println("Invalid Input") else break
+            }
+        }
+
+        count++
+    }
+
+}
+fun checkValidMove(move: String): Boolean {
+    val validSquaresRegex = "[a-h][1-8][a-h][1-8]".toRegex()
+    return validSquaresRegex.matches(move)
+}
+fun printTitle() {
     println("Pawns-Only Chess")
+}
+fun promptPlayer(label: String) : String{
+    println("$label Player's name:")
+    return readln()
+}
+fun printGameBoard(board: List<MutableList<String>>) {
+
     board.forEachIndexed(){ i, rank -> println(rank.joinToString(prefix = "  +---+---+---+---+---+---+---+---+\n${rank.size - i} | ", separator = " | ", postfix = " |"))}
     println("  +---+---+---+---+---+---+---+---+")
     println(('a'..'h').joinToString(prefix = "    ", separator = "   ", postfix = "  "))
